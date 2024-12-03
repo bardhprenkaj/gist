c1b78576802199827f1eef6eff017d7ef2dea652
Bardh Prenkaj <bardhprenkaj95@gmail.com>
2024-12-03 15:18:01 +0100

    torch base changed

--- src/core/torch_base.py
+++ src/core/torch_base.py
@@ -1,3 +1,4 @@
+from typing import Tuple, Union
 import numpy as np
 import random
 import torch
@@ -16,8 +17,8 @@ class TorchBase(Trainable):
         self.epochs = self.local_config['parameters']['epochs']
         self.batch_size = self.local_config['parameters']['batch_size']
         
-        self.model = get_instance_kvargs(self.local_config['parameters']['model']['class'],
-                                   self.local_config['parameters']['model']['parameters'])
+        self.model: Union[Trainable, torch.nn.Module] = get_instance_kvargs(self.local_config['parameters']['model']['class'],
+                                                                            self.local_config['parameters']['model']['parameters'])
         
         self.optimizer = get_instance_kvargs(self.local_config['parameters']['optimizer']['class'],
                                       {'params':self.model.parameters(), **self.local_config['parameters']['optimizer']['parameters']})
@@ -27,8 +28,7 @@ class TorchBase(Trainable):
         
         self.early_stopping_threshold = self.local_config['parameters']['early_stopping_threshold']
         
-        self.lr_scheduler =  lr_scheduler.LinearLR(self.optimizer, start_factor=1.0, end_factor=0.5, total_iters=self.epochs)
-
+        self.lr_scheduler = lr_scheduler.ReduceLROnPlateau(optimizer=self.optimizer, patience=10, factor=0.1)
         
         self.device = (
             "cuda"
@@ -39,91 +39,44 @@ class TorchBase(Trainable):
         )
         self.model.to(self.device) 
         
-        self.patience = 0                           
+        self.patience = 0                 
     
     def real_fit(self):
-              
         instances = self.dataset.get_torch_instances(fold_id=self.fold_id)
-        train_loader, val_loader = None, None
+        train_loader, val_loader = self.__get_loaders(instances)
         
-        if self.early_stopping_threshold:
-            num_instances = len(self.dataset.instances)
-            # get 5% of training instances and reserve them for validation
-            indices = list(range(num_instances))
-            random.shuffle(indices)
-            val_size = max(int(.05 * len(indices)), self.batch_size)
-            train_size = len(indices) - val_size
-            # get the training instances
-            train_instances = Subset(instances, indices[:train_size - 1])
-            val_instances = Subset(instances, indices[train_size:])
-            # get the train and validation loaders
-            train_loader = DataLoader(train_instances, batch_size=self.batch_size, shuffle=True, drop_last=True)
-            val_loader = DataLoader(val_instances, batch_size=self.batch_size, shuffle=True, drop_last=True)
-        else:
-            train_loader = DataLoader(instances, batch_size=self.batch_size, shuffle=True, drop_last=True)
+        best_val_loss =float('inf')
+        interrupt: bool = False
 
-        best_loss = [0,0]
-        
         for epoch in range(self.epochs):
-            losses, preds, labels_list = [], [], []
             self.model.train()
-            for batch in train_loader:
-                batch.batch = batch.batch.to(self.device)
-                node_features = batch.x.to(self.device)
-                edge_index = batch.edge_index.to(self.device)
-                edge_weights = batch.edge_attr.to(self.device)
-                labels = batch.y.to(self.device).long()
-                
-                self.optimizer.zero_grad()
-                
-                pred = self.model(node_features, edge_index, edge_weights, batch.batch)
-                loss = self.loss_fn(pred, labels)
-                losses.append(loss.to('cpu').detach().numpy())
-                loss.backward()
-                
-                labels_list += list(labels.squeeze().long().detach().to('cpu').numpy())
-                preds += list(pred.squeeze().detach().to('cpu').numpy())
-               
-                self.optimizer.step()
-
+            losses, preds, labels_list = self.fwd(train_loader)
             accuracy = self.accuracy(labels_list, preds)
-            self.context.logger.info(f'epoch = {epoch} ---> loss = {np.mean(losses):.4f}\t accuracy = {accuracy:.4f}')
-            self.lr_scheduler.step()
+            log_msg = f'Epoch = {epoch} ---> loss = {np.mean(losses):.4f}\t accuracy = {accuracy:.4f}'
+            self.lr_scheduler.step(np.mean(losses))
             
             # check if we need to do early stopping
-            if self.early_stopping_threshold and len(val_loader) > 0:
+            if len(val_loader) > 0:
                 self.model.eval()
-                var_losses, var_labels, var_preds = [], [], []
                 with torch.no_grad():
-                    for batch in val_loader:
-                        batch.batch = batch.batch.to(self.device)
-                        node_features = batch.x.to(self.device)
-                        edge_index = batch.edge_index.to(self.device)
-                        edge_weights = batch.edge_attr.to(self.device)
-                        labels = batch.y.to(self.device).long()
-
-                        pred = self.model(node_features, edge_index, edge_weights, batch.batch)
-                        loss = self.loss_fn(pred, labels)
-                        
-                        var_labels += list(labels.squeeze().to('cpu').numpy())
-                        var_preds += list(pred.squeeze().to('cpu').numpy())
-                        
-                        var_losses.append(loss.item())
-                        
-                    best_loss.pop(0)
-                    var_loss = np.mean(var_losses)
-                    best_loss.append(var_loss)
-                            
-                    accuracy = self.accuracy(var_labels, var_preds)
-                    self.context.logger.info(f'epoch = {epoch} ---> var_loss = {var_loss:.4f}\t var_accuracy = {accuracy:.4f}')
+                    val_losses, val_preds, val_labels = self.fwd(val_loader, val=True)
+                    val_loss = np.mean(val_losses)
+                    accuracy = self.accuracy(val_labels, val_preds)
+                    log_msg += f'\tvar_loss = {val_loss:.4f}\t var_accuracy = {accuracy:.4f}'
                 
-                if abs(best_loss[0] - best_loss[1]) < self.early_stopping_threshold:
+                if val_loss < best_val_loss:
+                    best_val_loss = val_loss
+                    self.patience = 0
+                else:
                     self.patience += 1
                     
-                    if self.patience == 4:
-                        self.context.logger.info(f"Early stopped training at epoch {epoch}")
-                        break  # terminate the training loop
-                
+                if self.early_stopping_threshold and self.patience == self.early_stopping_threshold:
+                    log_msg += f"Early stopped training at epoch {epoch}"
+                    interrupt = True  # terminate the training loop
+
+            self.context.logger.info(log_msg)
+            if interrupt: break
+
     def check_configuration(self):
         super().check_configuration()
         local_config=self.local_config
@@ -154,3 +107,48 @@ class TorchBase(Trainable):
             for model in self.model:
                 if isinstance(model, torch.nn.Module):
                     model.to(self.device)
+
+
+    def fwd(self, loader, val=False):
+        losses, preds, labels_list = [], [], []
+        for batch in loader:
+            batch.batch = batch.batch.to(self.device)
+            node_features = batch.x.to(self.device).to(torch.float)
+            edge_index = batch.edge_index.to(self.device).to(torch.int64)
+            edge_weights = batch.edge_attr.to(self.device).to(torch.float)
+            labels = batch.y.to(self.device).long()
+            
+            if not val:
+                self.optimizer.zero_grad()
+            
+            pred, loss = self.model.fwd(node_features, edge_index, edge_weights, batch.batch, labels, self.loss_fn)
+            losses.append(loss.to('cpu').detach().numpy())
+            if not val:
+                loss.backward()
+                self.optimizer.step()
+            
+            labels_list += list(labels.squeeze().long().detach().to('cpu').numpy())
+            preds += list(pred.squeeze().detach().to('cpu').numpy())
+        
+        return losses, preds, labels_list
+
+
+    def __get_loaders(self, instances) -> Tuple[DataLoader, DataLoader]:
+        train_loader, val_loader = None, None
+        if self.early_stopping_threshold:
+            num_instances = len(self.dataset.instances)
+            # get 5% of training instances and reserve them for validation
+            indices = list(range(num_instances))
+            random.shuffle(indices)
+            val_size = max(int(.05 * len(indices)), self.batch_size)
+            train_size = len(indices) - val_size
+            # get the training instances
+            train_instances = Subset(instances, indices[:train_size - 1])
+            val_instances = Subset(instances, indices[train_size:])
+            # get the train and validation loaders
+            train_loader = DataLoader(train_instances, batch_size=self.batch_size, shuffle=True, drop_last=True)
+            val_loader = DataLoader(val_instances, batch_size=self.batch_size, shuffle=True, drop_last=True)
+        else:
+            train_loader = DataLoader(instances, batch_size=self.batch_size, shuffle=True, drop_last=True)
+
+        return train_loader, val_loader
